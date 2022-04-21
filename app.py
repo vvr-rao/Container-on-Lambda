7bb78478322148a1765c4467630d07f8d31bf549
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2022-04-21 09:40:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 def handler(event, context):
     data = json.loads(json.dumps(event))
     payload = data['data']
-    output = "Hello " + Payload
+    output = "Hello " + payload
     Resp = [{"response": output}]
     response = {"statusCode": 200, "body": json.dumps(Resp)}
     return response
