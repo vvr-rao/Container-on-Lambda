71033fd34126eb658ca7106e256aafa19f2461fc
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2022-04-21 10:24:07 -0400

    Update and rename app.py to main.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import json
-def handler(event, context):
-    data = json.loads(json.dumps(event))
-    payload = data['data']
-    output = "Hello " + payload
-    Resp = [{"response": output}]
-    response = {"statusCode": 200, "body": json.dumps(Resp)}
-    return response
---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+import joblib
+
+def handler(event, context):
+    #import the model
+    model = open('model/SVM_cv_model.pkl','rb')
+    clf = joblib.load(model)
+    
+    #read the input string
+    data = json.loads(json.dumps(event))
+    payload = data['data']
+    
+    new_data = []
+    
+    string1 = payload.replace('\d+', '') # remove digits
+    string1 = string1.replace('[^\w\s]', '') # remove punctuation
+    new_data.append(string1)
+    
+    my_prediction = clf.predict(data)
+    
+    out = {1: 'Negative', 2: 'Neutral', 3: 'Positive'}
+    
+    output = print(out[0])
+    #Resp = [{"response": output}]
+    #response = {"statusCode": 200, "body": json.dumps(Resp)}
+    {"Sentiiment": output}
+    return response
