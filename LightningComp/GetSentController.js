81240dfab32e96d83da951f475ceea5b6918ef83
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2022-04-26 14:13:03 -0400

    Add files via upload

---
+++ LightningComp/GetSentController.js
@@ -0,0 +1,33 @@
+({
+	doInit : function(component, event, helper) {
+		
+        var action = component.get("c.serverEcho");
+        
+        action.setParams({ recordId : component.get("v.recordId") });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                //alert("From server: " + response.getReturnValue());
+                window.setTimeout(
+ 				 	$A.getCallback(function() {
+                 		//$A.get("e.force:closeQuickAction").fire(); 
+    			 		//$A.get('e.force:refreshView').fire();                 
+  				 		}), 
+                    500); // Waits 5 seconds
+                $A.get("e.force:closeQuickAction").fire(); 
+                $A.get('e.force:refreshView').fire();
+            }
+            else {
+                alert("Some Error!!")
+            }
+        });
+        
+        $A.enqueueAction(action);
+        //eval("$A.get('e.force:refreshView').fire();")
+        
+        // Close the action pane
+        
+        //$A.get("e.force:closeQuickAction").fire(); 
+        //$A.get("e.force:refreshView").fire();
+	}
+})
